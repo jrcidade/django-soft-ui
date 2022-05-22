48a63f05907d3ffe4958799431510ce63c4fee93
negaramp <negar.aminpour@yahoo.com>
2022-05-22 12:01:55 -0400

    test

--- apps/home/forms.py
+++ apps/home/forms.py
@@ -4,4 +4,4 @@ from .models import patient
 class patientForm(forms.ModelForm):
 	class Meta:
 		model = patient
-		fields = ['Firstname','Lastname', 'Age', 'Date',]
+		fields = ['Firstname','Lastname', 'Age', 'Date']
